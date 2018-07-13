042795ae1a6ca57a2c5c32e4bbd48c0bb9526d97
Ido Shamun <ido@elegantmonkeys.com>
2018-07-13 18:07:52 +0300

    feat: webhook function which receives superfeedr updates

---
+++ webhook/index.js
@@ -0,0 +1,77 @@
+const crypto = require('crypto');
+const PubSub = require(`@google-cloud/pubsub`);
+const pubsub = new PubSub();
+
+const convertTime = (epoch) => (epoch ? new Date(epoch) : null);
+
+const createOrGetTopic = () => {
+    const topicName = 'post-fetched';
+    return pubsub.createTopic(topicName)
+        .then((results) => {
+            const topic = results[0];
+            console.log(`topic ${topic.name} created`);
+            return topic;
+        })
+        .catch((err) => {
+            if (err.code === 6) {
+                return pubsub.topic(topicName);
+            } else {
+                console.error('failed to create topic', err);
+            }
+        });
+};
+
+exports.webhook = (req, res) => {
+    const pubId = req.url.substring(1);
+
+    if (req.method.toLowerCase() !== 'post' || !pubId.length) {
+        return res.status(404).send();
+    }
+
+    const contentType = req.get('content-type');
+    if (!contentType || contentType.indexOf('application/json') < -1) {
+        console.warn(`Unknown content-type "${contentType}"`);
+        return res.status(400).send('Unknown content-type');
+    }
+
+    console.log(`received notification from ${pubId}`);
+
+    const body = req.body;
+    console.log(body);
+
+    const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECRET);
+    const sig = `sha1=${hmac.update(JSON.stringify(body)).digest('hex')}`;
+    if (req.get('x-hub-signature') !== sig) {
+        console.warn('Failed to verify hub signature');
+        return res.status(401).send();
+    }
+
+    const items = body.items || [];
+    if (items) {
+        return createOrGetTopic()
+            .then((topic) => {
+                const publisher = topic.publisher({
+                    batching: {
+                        maxMessages: 10,
+                        maxMilliseconds: 1000,
+                    },
+                });
+
+                return Promise.all(items.map((item) => ({
+                    id: crypto.createHash('md5').update(item.id).digest('hex'),
+                    title: item.title,
+                    categories: item.categories ? item.categories : [],
+                    publishedAt: convertTime(item.published),
+                    updatedAt: convertTime(item.updated),
+                    publicationId: pubId,
+                    url: item.permalinkUrl.split('?').shift(),
+                })).map((item) => {
+                    console.log('publishing post', item);
+                    return publisher.publish(Buffer.from(JSON.stringify(item)));
+                }));
+            })
+            .then(() => res.status(200).send('OK'));
+    }
+
+    res.status(200).send('OK');
+};
---
+++ webhook/utils.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const request = require('request');
+
+const secret = process.env.WEBHOOK_SECRET;
+const webhook = process.env.WEBHOOK_URL;
+const user = process.env.USER;
+const pass = process.env.PASS;
+
+
+const sendOnePayload = (payloads) => {
+    if (!payloads.length) {
+        console.log('done!');
+        process.exit();
+    }
+
+    request({
+        url: 'https://push.superfeedr.com/',
+        method: 'POST',
+        form: payloads[0],
+        auth: {
+            user,
+            pass,
+            sendImmediately: true,
+        },
+    }, (err) => {
+        if (err) {
+            console.error(`failed to add ${payloads[0]['hub.topic']}`);
+            process.exit(1);
+        } else {
+            sendOnePayload(payloads.slice(1));
+        }
+    });
+};
+
+const payloads = fs.readFileSync('sources.csv')
+    .toString()
+    .split('\n')
+    .map((line) => {
+        const cols = line.split(',');
+        return {
+            'hub.mode': 'subscribe',
+            'hub.topic': cols[1],
+            'hub.callback': `${webhook}/${cols[0]}`,
+            'hub.secret': secret,
+            format: 'json',
+        };
+    });
+
+sendOnePayload(payloads);
