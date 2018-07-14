7a0461e56860fd3d66531b07fa4ad4eded60c439
Ido Shamun <ido@elegantmonkeys.com>
2018-07-14 11:53:49 +0300

    chore: add editorconfig and reformat everything

--- crawler/index.js
+++ crawler/index.js
@@ -4,79 +4,79 @@ const PubSub = require(`@google-cloud/pubsub`);
 const pubsub = new PubSub();
 
 const rules = [
-    require('metascraper-date')(),
-    require('metascraper-title')(),
-    require('metascraper-url')(),
-    require('./rules')(),
+  require('metascraper-date')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('./rules')(),
 ];
 
 const createOrGetTopic = () => {
-    const topicName = 'crawled-post';
-    return pubsub.createTopic(topicName)
-        .then((results) => {
-            const topic = results[0];
-            console.log(`topic ${topic.name} created`);
-            return topic;
-        })
-        .catch((err) => {
-            if (err.code === 6) {
-                return pubsub.topic(topicName);
-            } else {
-                console.error('failed to create topic', err);
-            }
-        });
+  const topicName = 'crawled-post';
+  return pubsub.createTopic(topicName)
+    .then((results) => {
+      const topic = results[0];
+      console.log(`topic ${topic.name} created`);
+      return topic;
+    })
+    .catch((err) => {
+      if (err.code === 6) {
+        return pubsub.topic(topicName);
+      } else {
+        console.error('failed to create topic', err);
+      }
+    });
 };
 
 const extractMetaTags = (url) =>
-    request(url)
-        .then((html) => {
-            const htmlDom = cheerio.load(html);
-            return rules.reduce((acc, rule) => {
-                const newRes = Object.keys(rule).reduce((acc2, key) => {
-                    for (let i = 0; i < rule[key].length; i++) {
-                        const res = rule[key][i]({ htmlDom, url });
-                        if (res) {
-                            return Object.assign({}, acc2, { [key]: res });
-                        }
-                    }
+  request(url)
+    .then((html) => {
+      const htmlDom = cheerio.load(html);
+      return rules.reduce((acc, rule) => {
+        const newRes = Object.keys(rule).reduce((acc2, key) => {
+          for (let i = 0; i < rule[key].length; i++) {
+            const res = rule[key][i]({ htmlDom, url });
+            if (res) {
+              return Object.assign({}, acc2, { [key]: res });
+            }
+          }
 
-                    return acc2;
-                }, {});
-                return Object.assign({}, acc, newRes);
-            }, {});
-        });
+          return acc2;
+        }, {});
+        return Object.assign({}, acc, newRes);
+      }, {});
+    });
 
 const convertTagsToSchema = (tags) => {
-    const obj = Object.assign({}, tags);
-    if (obj.date) {
-        obj.publishedAt = new Date(obj.date);
-        delete obj.date;
-    }
-    if (obj.modified) {
-        obj.updatedAt = new Date(obj.modified);
-        delete obj.modified;
-    }
-    return obj;
+  const obj = Object.assign({}, tags);
+  if (obj.date) {
+    obj.publishedAt = new Date(obj.date);
+    delete obj.date;
+  }
+  if (obj.modified) {
+    obj.updatedAt = new Date(obj.modified);
+    delete obj.modified;
+  }
+  return obj;
 };
 
 exports.crawler = (event) => {
-    const pubsubMessage = event.data;
-    const data = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
+  const pubsubMessage = event.data;
+  const data = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
 
-    console.log(`[${data.id}] scraping ${data.url} to enrich`, data);
+  console.log(`[${data.id}] scraping ${data.url} to enrich`, data);
 
-    return extractMetaTags(data.url)
-        .then(convertTagsToSchema)
-        .then(tags => Object.assign({}, data, tags, data.title ? { title: data.title } : {}))
-        .catch((err) => {
-            console.error(`[${data.id}] failed to scrape ${data.url}`, err);
-            return data;
+  return extractMetaTags(data.url)
+    .then(convertTagsToSchema)
+    .then(tags => Object.assign({}, data, tags, data.title ? { title: data.title } : {}))
+    .catch((err) => {
+      console.error(`[${data.id}] failed to scrape ${data.url}`, err);
+      return data;
+    })
+    .then((item) =>
+      createOrGetTopic()
+        .then((topic) => {
+          console.log(`[${data.id}] crawled post`, item);
+          return topic.publisher().publish(Buffer.from(JSON.stringify(item)));
         })
-        .then((item) =>
-            createOrGetTopic()
-                .then((topic) => {
-                    console.log(`[${data.id}] crawled post`, item);
-                    return topic.publisher().publish(Buffer.from(JSON.stringify(item)));
-                })
-        );
+    );
 };
--- webhook/utils.js
+++ webhook/utils.js
@@ -10,42 +10,42 @@ const pass = process.env.PASS;
 
 
 const sendOnePayload = (payloads) => {
-    if (!payloads.length) {
-        console.log('done!');
-        process.exit();
-    }
+  if (!payloads.length) {
+    console.log('done!');
+    process.exit();
+  }
 
-    request({
-        url: 'https://push.superfeedr.com/',
-        method: 'POST',
-        form: payloads[0],
-        auth: {
-            user,
-            pass,
-            sendImmediately: true,
-        },
-    }, (err) => {
-        if (err) {
-            console.error(`failed to add ${payloads[0]['hub.topic']}`);
-            process.exit(1);
-        } else {
-            sendOnePayload(payloads.slice(1));
-        }
-    });
+  request({
+    url: 'https://push.superfeedr.com/',
+    method: 'POST',
+    form: payloads[0],
+    auth: {
+      user,
+      pass,
+      sendImmediately: true,
+    },
+  }, (err) => {
+    if (err) {
+      console.error(`failed to add ${payloads[0]['hub.topic']}`);
+      process.exit(1);
+    } else {
+      sendOnePayload(payloads.slice(1));
+    }
+  });
 };
 
 const payloads = fs.readFileSync('sources.csv')
-    .toString()
-    .split('\n')
-    .map((line) => {
-        const cols = line.split(',');
-        return {
-            'hub.mode': 'subscribe',
-            'hub.topic': cols[1],
-            'hub.callback': `${webhook}/${cols[0]}`,
-            'hub.secret': secret,
-            format: 'json',
-        };
-    });
+  .toString()
+  .split('\n')
+  .map((line) => {
+    const cols = line.split(',');
+    return {
+      'hub.mode': 'subscribe',
+      'hub.topic': cols[1],
+      'hub.callback': `${webhook}/${cols[0]}`,
+      'hub.secret': secret,
+      format: 'json',
+    };
+  });
 
 sendOnePayload(payloads);
