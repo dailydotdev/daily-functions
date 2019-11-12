d65243f7f26418833a543961938a7d8192a6eacf
Ido Shamun <ido@elegantmonkeys.com>
2019-11-13 00:58:15 +0200

    fix(crawler): merge tag vue.js with vue

--- imager/utils.js
+++ imager/utils.js
@@ -1,16 +1,16 @@
 const crypto = require('crypto');
 const PubSub = require(`@google-cloud/pubsub`);
 const pubsub = new PubSub();
-// const topic = pubsub.topic('crawled-post');
-const topic = pubsub.topic('ad-image-processed');
+const topic = pubsub.topic('crawled-post');
+// const topic = pubsub.topic('ad-image-processed');
 
-const link = 'https://www.producthunt.com/posts/daily-2-0';
-const description = 'Daily 2.0 is now live on Product Hunt 🚀, help us to grow!🤯';
+const link = 'https://github.com/dailynowco/daily';
+const description = 'We are 100% open source and looking for all sorts of contribution 🤓 ';
 const source = 'Daily';
-const image = 'https://storage.googleapis.com/devkit-assets/ads/daily2.gif';
-const start = new Date('4/30/19Z');
+const image = 'https://storage.googleapis.com/devkit-assets/ads/oss_wide.jpg';
+const start = new Date('8/10/19Z');
 // const end = new Date(start.getTime() + 7 * 24 * 60 * 60 * 1000);
-const end = new Date('4/31/19Z');
+const end = new Date('8/10/20Z');
 
 const data = {
   id: crypto.createHash('md5').update(link).digest('hex'),
