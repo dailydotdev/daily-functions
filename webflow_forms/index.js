7b9d9b693fb86e63542fa7f1e7dea6ae7bf1adc7
Ido Shamun <ido@elegantmonkeys.com>
2020-04-19 12:23:31 +0300

    feat: new function to integrate webflow with sendgrid
    The new function is a webhook which is triggered by a new form submission in webflow and adds the contact to sendgrid

---
+++ webflow_forms/index.js
@@ -0,0 +1,48 @@
+const client = require('@sendgrid/client');
+client.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.webflow_forms = (req, res) => {
+  if (req.method.toLowerCase() !== 'post' || req.url !== '/') {
+    return res.status(404).send();
+  }
+
+  const contentType = req.get('content-type');
+  if (!contentType || contentType.indexOf('application/json') < -1) {
+    console.warn(`unknown content-type "${contentType}"`);
+    return res.status(400).send();
+  }
+
+  const body = req.body;
+  if (body.site !== process.env.WEBFLOW_SITE) {
+    return res.status(400).send();
+  }
+
+  let listId;
+  switch (body.name) {
+    case 'Newsletter':
+      listId = 'f439c1ff-6027-4e09-a109-2a0ac27f7903';
+      break;
+    default:
+      return res.status(400).send();
+  }
+
+  return client.request({
+    method: 'PUT',
+    url: '/v3/marketing/contacts',
+    body: {
+      list_ids: [listId],
+      contacts: [{
+        email: body.data.Email,
+      }],
+    },
+  })
+    .then(([contactsRes, contactsBody]) => {
+      if (contactsRes.statusCode === 202) {
+        console.log(`added ${body.data.Email} to ${body.name}`);
+        return res.status(200).send('OK');
+      }
+      console.error(contactsBody);
+      return res.status(500).send();
+    })
+    .catch(() => res.status(500).send());
+};
